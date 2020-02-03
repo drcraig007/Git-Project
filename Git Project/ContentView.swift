2a1240107b6de7c2841cf5f758ecd116623b7de6
Dr. Craig Curphey <drcraigceo@gmail.com>
2020-02-03 14:56:42 -0500

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Dr Craig E Curphey on 2020-02-03.
+//  Copyright © 2020 Dr Craig E Curphey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
